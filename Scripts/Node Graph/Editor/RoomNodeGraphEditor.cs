a3e4ff97b1c7dd441cad77b7680bb8bd63977282
OBGcancode <owenbawekgarcia@gmail.com>
2022-06-23 09:13:32 -0700

    Original Project

---
+++ Scripts/Node Graph/Editor/RoomNodeGraphEditor.cs
@@ -0,0 +1,209 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System;
+
+public class RoomNodeGraphEditor : EditorWindow
+{
+    
+    private GUIStyle roomNodeStyle;
+    private GUIStyle roomNodeSelectedStyle;
+    private static RoomNodeGraphSO currentRoomNodeGraph;
+    private const float nodeWidth = 160f;
+    private RoomNodeTypeListSO roomNodeTypeList;
+    
+
+    private const float nodeHeight = 75f;
+    private const int nodePadding = 25;
+    private const int nodeBorder = 12;
+    
+    
+    
+    [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
+
+
+    private static void OpenWindow () 
+    {
+        GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
+
+    private void OnEnable()
+    {
+        // Define node layout style
+        roomNodeStyle = new GUIStyle();
+        roomNodeStyle.normal.background = EditorGUIUtility.Load("node1") as Texture2D;
+        roomNodeStyle.normal.textColor = Color.white;
+        roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
+        roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+
+        //Load Room node types
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeListSO;
+
+
+    }
+
+
+
+
+
+    /// <summary>
+    /// Open the room node graph editor window if a room node graph scriptable object asset is double clicked in the inspector
+    /// </summary>
+
+    [OnOpenAsset(0)]  // Need the namespace UnityEditor.Callbacks
+    public static bool OnDoubleClickAsset(int instanceID, int line)
+    {
+        RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
+
+        if (roomNodeGraph != null)
+        {
+            OpenWindow();
+
+            currentRoomNodeGraph = roomNodeGraph;
+
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+   
+    
+    
+    
+    
+    
+    
+    
+    /// <summary> 
+    /// <Draw Editor GUI>
+    /// </summary>
+    private void OnGUI() 
+    {
+        // If a SO of type RoomNodeGraphSO has been slected then proceed
+        if (currentRoomNodeGraph != null) 
+        {
+            //Process Events 
+            ProcessEvents(Event.current);
+
+
+
+
+            //Draw Room Nodes
+            DrawRoomNodes();
+
+        }
+
+        if (GUI.changed)
+        {
+            Repaint();
+        }
+
+
+
+
+    }
+
+    private void ProcessEvents(Event currentEvent)
+    {
+        ProcessRoomNodeGraphEvents(currentEvent);
+    }
+    /// <summary>
+    /// Process Room Node Graph Events
+    /// </summary>
+    private void ProcessRoomNodeGraphEvents(Event currentEvent)
+    {
+        switch (currentEvent.type)
+        {
+            
+            
+            
+            // Process Mouse Down Events
+            case EventType.MouseDown:
+                ProcessMouseDownEvent(currentEvent);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Process mouse down events on the room node graph (not over a node)
+    /// </summary>
+    private void ProcessMouseDownEvent(Event currentEvent)
+    {
+        // Process right click mouse down on graph event (show context menu)
+        if (currentEvent.button == 1)
+        {
+            ShowContextMenu(currentEvent.mousePosition);
+        }
+       
+    }
+    /// <summary>
+    /// Show the context menu
+    /// </summary>
+    private void ShowContextMenu(Vector2 mousePosition)
+    {
+        GenericMenu menu = new GenericMenu();
+
+        menu.AddItem(new GUIContent("Create Room Node"), false, CreateRoomNode, mousePosition);
+        
+
+        menu.ShowAsContext();
+    }
+    /// <summary>
+    /// Create a room node at the mouse position
+    /// </summary>
+    private void CreateRoomNode(object mousePositionObject)
+    {
+        CreateRoomNode(mousePositionObject, roomNodeTypeList.list.Find(x => x.isNone));
+    }
+    /// <summary>
+    /// Create a room node at the mouse position - overloaded to also pass in RoomNodeType
+    /// </summary>
+    private void CreateRoomNode(object mousePositionObject, RoomNodeTypeSO roomNodeType)
+    {
+        Vector2 mousePosition = (Vector2)mousePositionObject;
+
+        // create room node scriptable object asset
+        RoomNodeSO roomNode = ScriptableObject.CreateInstance<RoomNodeSO>();
+
+        // add room node to current room node graph room node list
+        currentRoomNodeGraph.roomNodeList.Add(roomNode);
+
+        // set room node values
+        roomNode.Initialise(new Rect(mousePosition, new Vector2(nodeWidth, nodeHeight)), currentRoomNodeGraph, roomNodeType);
+
+        // add room node to room node graph scriptable object asset database
+        AssetDatabase.AddObjectToAsset(roomNode, currentRoomNodeGraph);
+
+        AssetDatabase.SaveAssets();
+    }
+    /// <summary>
+    /// Draw room nodes in the graph window
+    /// </summary>
+    private void DrawRoomNodes()
+    {
+        // Loop through all room nodes and draw themm
+        foreach (RoomNodeSO roomNode in currentRoomNodeGraph.roomNodeList)
+        {
+            roomNode.Draw(roomNodeStyle);
+        }
+
+        GUI.changed = true;
+    }
+
+
+
+
+
+
+}
---
+++ Scripts/Node Graph/RoomNodeSO.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class RoomNodeSO : ScriptableObject
+{
+    [HideInInspector] public string id;
+    [HideInInspector] public List<string> parentRoomNodeIDList = new List<string>();
+    [HideInInspector] public List<string> childroomNodeIDList = new List<string>();
+    [HideInInspector] public RoomNodeGraphSO roomNodeGraph;
+    public RoomNodeTypeSO roomNodeType;
+    [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
+
+
+    #region Editor Code
+
+
+
+    //following code should only run in the editor 
+#if UNITY_EDITOR
+    [HideInInspector] public Rect rect;
+
+
+    /// <summary>
+    /// Initialise node
+    /// </summary>
+    public void Initialise(Rect rect, RoomNodeGraphSO nodeGraph, RoomNodeTypeSO roomNodeType)
+    {
+        this.rect = rect;
+        this.id = Guid.NewGuid().ToString();
+        this.name = "RoomNode";
+        this.roomNodeGraph = nodeGraph;
+        this.roomNodeType = roomNodeType;
+
+        // Load room node type list
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeListSO;
+    }
+    /// <summary>
+    /// Draw node with node style
+    ///  </summary>
+    public void Draw (GUIStyle nodeStyle)
+    {
+        //Draw Node box using begin area
+        GUILayout.BeginArea(rect, nodeStyle);
+
+        // Start Region to detect popup slection changes 
+        EditorGUI.BeginChangeCheck();
+
+        //Display a popup using the RoomNodeType name values that can be selected from (default to the currently set roomNodeType)
+
+        int selected = roomNodeTypeList.list.FindIndex(x => x == roomNodeType);
+
+        int selection = EditorGUILayout.Popup("", selected, GetRoomNodeTypesToDisplay());
+
+        roomNodeType = roomNodeTypeList.list[selection];
+
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            EditorUtility.SetDirty(this);
+        }
+        GUILayout.EndArea();
+
+    }
+
+    /// <summary>
+    /// Populate a string array with the room node types to display that can be selected
+    /// </summary>
+    public string[] GetRoomNodeTypesToDisplay()
+    {
+        string[] roomArray = new string[roomNodeTypeList.list.Count];
+
+        for (int i = 0; i < roomNodeTypeList.list.Count; i++)
+        {
+            if (roomNodeTypeList.list[i].displayInNodeGraphEditor)
+            {
+                roomArray[i] = roomNodeTypeList.list[i].roomNodeTypeName;
+            }
+        }
+
+        return roomArray;
+    }
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+    #endregion Editor Code
+}
+
+
